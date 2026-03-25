b1b30600484f710a13379700020ea8c58376923f
Neeraj <neerajhp3@gmail.com>
2026-03-25 22:23:17 +0530

    first commit

---
+++ src/gitproject2/java1.java
@@ -0,0 +1,15 @@
+package gitproject2;
+
+public class java1 {
+	 public static void main(String[] args) {
+	        int a = 10;
+	        int b = 20;
+
+	        int sum = a + b;
+
+	        System.out.println("Sum = " + sum);
+	        
+	        
+	    }
+
+}
